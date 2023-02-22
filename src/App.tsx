8807177ee959bb127fc0d90f0af4c980f7a2fb08
PaulMarv <mpsalako@gmail.com>
2023-02-22 22:21:38 +0100

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Container } from 'react-bootstrap';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Store } from './pages/Store';
+import { Navbar } from './components/Navbar';
+
 
 function App() {
   
 
   return (
-    <Container>
+    <>
+    <Navbar/>
+    <Container className='mb-4'>
       <Routes>
         <Route path='/' element={<Home />}></Route>
 
@@ -17,6 +21,7 @@ function App() {
         <Route path='/about' element={<About />}></Route>
       </Routes>
     </Container>
+    </>
   )
 }
 
